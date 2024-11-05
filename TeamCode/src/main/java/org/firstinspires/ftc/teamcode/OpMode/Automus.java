47f5883eec5260801e8b2b98b04566780b8c541c
Naveen <57783716+Naveeaen@users.noreply.github.com>
2024-11-05 17:02:07 -0500

    added somw telepeo logica

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Automus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Automus.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.OpMode;
 
-public class Automus {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.pandaPathing.follower.Follower;
+import org.firstinspires.ftc.teamcode.pandaPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pandaPathing.pathGeneration.BezierLine;
+import org.firstinspires.ftc.teamcode.pandaPathing.pathGeneration.Path;
+import org.firstinspires.ftc.teamcode.pandaPathing.pathGeneration.Point;
+
+@Autonomous(name = "Automus")
+public class Automus extends LinearOpMode {
+    private Follower robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Follower(hardwareMap);
+        robot.initialize();
+        for (DcMotorEx motor : robot.motors)
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+        Point startPos = new Point(new Pose(0, 0, 0));
+        Point endPos = new Point(new Pose(0, 20, 0));
+        Path path1 = new Path(new BezierLine(startPos, endPos));
+        robot.followPath(path1);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TelePOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TelePOP.java
@@ -1,24 +1,20 @@
 package org.firstinspires.ftc.teamcode.OpMode;
 
-import androidx.loader.content.Loader;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.pandaPathing.follower.Follower;
-import org.firstinspires.ftc.teamcode.pandaPathing.util.SingleRunAction;
 
 import java.util.*;
 
 @Config
-@TeleOp(name = "Two Person Drive", group = "Drive")
+@TeleOp(name = "Telepepepop", group = "Drive")
 public class TelePOP extends OpMode {
     private Follower robot = new Follower(hardwareMap);
 
@@ -35,6 +31,9 @@ public class TelePOP extends OpMode {
     public static double f = 0;
     public static int target = 0;
     private final double TICKS_PER_DEG = 103.8/360;
+
+    boolean dUpPressed, dDownPressed, yPressed, aPressed;
+    double strafePow;
 
     public void init() {
         // motor configs
@@ -72,19 +71,51 @@ public class TelePOP extends OpMode {
 
     public void loop(){
         // driving
-        robot.setTeleOpMovementVectors(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
+        strafePow = gamepad2.left_trigger != 0 ? -gamepad2.left_trigger : gamepad2.right_trigger;
+        robot.setTeleOpMovementVectors(-gamepad1.left_stick_y, strafePow, -gamepad1.right_stick_x);
         robot.update();
 
-        // correction math!
+        // calculate the slide power using pid
         slideyController.setPID(p, i, d);
         int slidePos = frontSlides.getCurrentPosition();
         double pid = slideyController.calculate(slidePos, target);
         double ff = Math.cos(Math.toRadians(target / TICKS_PER_DEG)) * f;
-
         double power = pid + ff;
 
-        frontSlides.setPower(power); // set both to the same power -->
-        backSlides.setPower(power);  // as the front encoder calculation
+        // change slide target based on controller control
+        if(gamepad2.left_stick_y != 0) {
+            if (gamepad2.left_stick_y > 0 && slidePos < 1000) // upper limit
+                target += 10 * gamepad2.left_stick_y;
+            if (gamepad2.left_stick_y < 0 && slidePos > 0) // lower limit
+                target += 10 * gamepad2.left_stick_y;
+            frontSlides.setPower(power);
+            backSlides.setPower(power);
+        } else {
+            if (gamepad2.dpad_up && !dUpPressed) {
+                target = 700; // top basket
+                dUpPressed = true;
+            } else if (!gamepad2.dpad_up) dUpPressed = false;
+            if (gamepad2.dpad_down && !dDownPressed) {
+                target = 0; // bottom
+                dDownPressed = true;
+            } else if (!gamepad2.dpad_down) dDownPressed = false;
+        }
+
+        // four bar
+        if(gamepad2.y && !yPressed){
+            drv4bR.setPosition(1);
+            drv4bL.setPosition(1);
+            yPressed = true;
+        } else if(!gamepad2.y){
+            yPressed = false;
+        }
+        if(gamepad2.a && !aPressed){
+            drv4bR.setPosition(0);
+            drv4bL.setPosition(0);
+            aPressed = true;
+        } else if(!gamepad2.a){
+            aPressed = false;
+        }
 
         telemetry.addData("pos", slidePos);
         telemetry.addData("target", target);
