42d61595b57558af796b6ac40f53a462b6f4a885
MCSpoooof <cachemaola@gmail.com>
2024-11-19 09:12:32 -0500

    fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Automus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Automus.java
@@ -29,10 +29,19 @@ public class Automus extends LinearOpMode {
             motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         Point startPos = new Point(0, 0, Point.CARTESIAN);
-        Point endPos = new Point(24, 0, Point.CARTESIAN);
-        Path path1 = new Path(new BezierLine(startPos, endPos));
+        Point Pos1 = new Point(28, 0, Point.CARTESIAN);
+        Point Pos2 = new Point(22, -10, Point.CARTESIAN);
+        Point Pos3 = new Point(40, -15, Point.CARTESIAN);
+        Point Pos4 = new Point(10, -15, Point.CARTESIAN);
+        Path path1 = new Path(new BezierLine(startPos, Pos1));
+        Path path2 = new Path(new BezierLine(Pos1, Pos2));
+        Path path3 = new Path(new BezierLine(Pos2, Pos3));
+        Path path4 = new Path(new BezierLine(Pos3, Pos4));
         path1.setConstantHeadingInterpolation(0);
         robot.followPath(path1);
+        robot.followPath(path2);
+        robot.followPath(path3);
+        //robot.followPath(path4);
     }
     public void runOpMode(){
         initi();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pandaPathing/tuning/FollowerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pandaPathing/tuning/FollowerConstants.java
@@ -36,11 +36,11 @@ public class FollowerConstants {
     public static Vector frontLeftVector = MathFunctions.normalizeVector(new Vector(convertToPolar[0], convertToPolar[1]));
 
 
-    // Translational PIDF coefficients (don't use integral)
+    // Translational PIDF coefficients   (don't use integral)
     public static PIDFSet translationalPIDFCoefficients = new PIDFSet(
-            0.17,
+            0.3,
             0,
-            0.009,
+            0.05,
             0);
 
     // Translational Integral
@@ -56,10 +56,10 @@ public class FollowerConstants {
 
     // Heading error PIDF coefficients
     public static PIDFSet headingPIDFCoefficients = new PIDFSet(
-            1,
+            0.95,
             0,
-            0.08,
-            0.005);
+            0.07,
+            0);
 
     // Feed forward constant added on to the heading PIDF
     public static double headingPIDFFeedForward = 0.01;
@@ -67,14 +67,14 @@ public class FollowerConstants {
 
     // Drive PIDF coefficients
     public static CustomFilteredPIDFCoefficients drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
-            0.0078,
+            0.01,
             0,
-            0.00000005,
-            1,
-            0.019);
+            0.00005,
+            0.6,
+            0);
 
     // Feed forward constant added on to the drive PIDF
-    public static double drivePIDFFeedForward = 0.01;
+    public static double drivePIDFFeedForward = 0.005;
 
     // Kalman filter parameters for the drive error Kalman filter
     public static KalmanFilterParameters driveKalmanFilterParameters = new KalmanFilterParameters(
@@ -86,7 +86,7 @@ public class FollowerConstants {
     public static double mass = 11.2 + 0.387 + 0.387;
 
     // Centripetal force to power scaling
-    public static double centripetalScaling = 0.0006;
+        public static double centripetalScaling = 0.00001;
 
 
     // Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
@@ -104,7 +104,7 @@ public class FollowerConstants {
     // Decreasing this will cause the deceleration at the end of the Path to be slower, making the
     // robot slower but reducing risk of end-of-path overshoots or localization slippage.
     // This can be set individually for each Path, but this is the default.
-    public static double zeroPowerAccelerationMultiplier = 4;
+    public static double zeroPowerAccelerationMultiplier = 3.5;
 
 
     // When the robot is at the end of its current Path or PathChain and the velocity goes below
@@ -130,7 +130,7 @@ public class FollowerConstants {
     // When the Path is considered at its end parametrically, then the Follower has this many
     // milliseconds to further correct by default.
     // This can be custom set for each Path.
-    public static double pathEndTimeoutConstraint = 500;
+    public static double pathEndTimeoutConstraint = 650;
 
     // This is how many steps the BezierCurve class uses to approximate the length of a BezierCurve.
     public static int APPROXIMATION_STEPS = 1000;
@@ -167,7 +167,7 @@ public class FollowerConstants {
     public static PIDFSet secondaryTranslationalPIDFCoefficients = new PIDFSet(
             0.3,
             0,
-            0.001,
+            0.03,
             0);
 
     // Secondary translational Integral value
@@ -186,9 +186,9 @@ public class FollowerConstants {
 
     // Secondary heading error PIDF coefficients
     public static PIDFSet secondaryHeadingPIDFCoefficients = new PIDFSet(
-            1,
+            1.3,
             0,
-            0,
+            0.2,
             0);
 
     // Feed forward constant added on to the secondary heading PIDF
@@ -200,10 +200,10 @@ public class FollowerConstants {
 
     // Secondary drive PIDF coefficients
     public static CustomFilteredPIDFCoefficients secondaryDrivePIDFCoefficients = new CustomFilteredPIDFCoefficients(
-            0.02,
+            0.013,
             0,
-            0.000003,
-            1,
+            0.0003,
+            0.6,
             0);
 
     // Feed forward constant added on to the secondary drive PIDF
